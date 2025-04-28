e93e4c450fec36276efa727f05595259e7e6a11c
Mahmut Tunahan Aktaş <mahmuttunahan0029@gmail.com>
2025-04-28 11:38:35 +0300

    last

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -8,7 +8,7 @@ import Carousel from '@/components/services/Carousel';
 export default function page() {
   return (
     <Fade in={true} timeout={500}>
-      <div className='h-screen w-full flex items-center justify-center'>
+      <div className='h-screen w-full flex items-center justify-center bg-gray-100 p-24'>
         <Carousel></Carousel>
       </div>
     </Fade>
--- src/components/services/Carousel.tsx
+++ src/components/services/Carousel.tsx
@@ -10,37 +10,54 @@ import 'swiper/css/pagination';
 
 export default function Carousel() {
     return (
-            <div className="w-full">
-                <Swiper
-                    effect={'cards'}
-                    grabCursor={true}
-                    pagination={{ clickable: true }}
-                    modules={[EffectCards, Pagination]}
-                    className="mySwiper">
+        <div className="w-full">
+            <Swiper
+                effect={'cards'}
+                grabCursor={true}
+                pagination={{ clickable: true }}
+                modules={[EffectCards, Pagination]}
+                className="mySwiper">
 
-                    <SwiperSlide className="bg-sky-400 rounded-3xl flex">
-                        <div className="relative w-full h-screen">
-                            <Image
-                                src="/images/services1.jpeg" // public klasöründe olmalı
-                                alt="Shelby Background"
-                                layout="fill"
-                                
-                                objectFit="cover"
-                                priority
-                            />
+                <SwiperSlide className="flex justify-center items-center p-12">
+                    <div className="relative w-full max-w-[calc(100%-2rem)] h-[calc(100vh-2rem)] rounded-3xl overflow-hidden">
+                        <Image
+                            src="/images/services1.jpeg" 
+                            alt="Shelby Background"
+                            layout="fill"
+                            objectFit="cover"
+                            priority
+                        />
 
-                            <div className="absolute bottom-0 w-full p-6 flex flex-col items-center justify-center">
-                                <div className='flex items-center bg-white rounded-3xl w-full gap-3 py-6 px-3'>
-                                    <h1 className='text-3xl font-bold'>RegeditPos</h1>
-                                    <p>Lorem, ipsum dolor sit amet consectlorum. Eligendi libero quae, iste quo tenetur atque rerum vel amet pariatur.</p>
-
-                                </div>
+                        <div className="absolute bottom-0 w-full p-6 flex flex-col items-center justify-center bg-gradient-to-t from-black via-transparent to-transparent rounded-b-3xl">
+                            <div className='bg-white rounded-3xl w-full max-w-3xl p-6 text-center'>
+                                <h1 className='text-3xl font-bold mb-4'>RegeditPos</h1>
+                                <p>Lorem, ipsum dolor sit amet consectlorum. Eligendi libero quae, iste quo tenetur atque rerum vel amet pariatur.</p>
                             </div>
                         </div>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide className="flex justify-center items-center p-12">
+                    <div className="relative w-full max-w-[calc(100%-2rem)] h-[calc(100vh-2rem)] rounded-3xl overflow-hidden">
+                        <Image
+                            src="/images/shelby.jpg" 
+                            alt="Shelby Background"
+                            layout="fill"
+                            objectFit="cover"
+                            priority
+                        />
+
+                        <div className="absolute bottom-0 w-full p-6 flex flex-col items-center justify-center bg-gradient-to-t from-black via-transparent to-transparent rounded-b-3xl">
+                            <div className='bg-white rounded-3xl w-full max-w-3xl p-6 text-center'>
+                                <h1 className='text-3xl font-bold mb-4'>RegeditPos</h1>
+                                <p>Lorem, ipsum dolor sit amet consectlorum. Eligendi libero quae, iste quo tenetur atque rerum vel amet pariatur.</p>
+                            </div>
+                        </div>
+                    </div>
+                </SwiperSlide>
 
 
-                    </SwiperSlide>
-                </Swiper>
-            </div>
+
+            </Swiper>
+        </div>
     );
 }
