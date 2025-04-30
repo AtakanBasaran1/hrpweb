eacc6347dbf7cf1c3fde48c43713819c0d29254b
Kaytiue7 <Kaytiue7@users.noreply.github.com>
2025-04-30 14:05:15 +0300

    update

--- src/components/contact/form.tsx
+++ src/components/contact/form.tsx
@@ -1,10 +1,73 @@
+'use client';
 import React from 'react';
+import { useState } from "react";
+import emailjs from "emailjs-com";
+import { time } from "console";
 import { FaLinkedin, FaInstagram } from 'react-icons/fa';
+import { p } from 'framer-motion/client';
 
 export default function Form () {
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        subject: "",
+        message: "",
+        phone: "",
+      });
+    
+      const [submissionTime, setSubmissionTime] = useState<string | null>(null);  
+    
+      const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+      };
+    
+      const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        const { name, email, subject, message, phone } = formData;
+ 
+        if (!name || !email || !subject || !message || !phone) {
+            alert("Lütfen tüm alanları doldurun.");
+            return;
+        }
+        
+        const currentTime = new Date().toLocaleString();  
+        setSubmissionTime(currentTime); 
+
+        
+    
+        emailjs
+          .send(
+            "service_3fj7cmb", // EmailJS servis ID'si
+            "template_iuc7iy6", // EmailJS template ID'si
+            { 
+              title: formData.subject,
+              name: formData.name,
+              time: currentTime,
+              message: formData.message,
+              email: formData.email,
+              phone: formData.phone,
+            },
+            "oX09rEWUpbyJHSpoe" // EmailJS public key
+          )
+          .then(
+            (response) => {
+              alert(`Mesajınız başarıyla gönderildi! Gönderim Zamanı: ${currentTime}`);
+              setFormData({ name: "", email: "", subject: "", message: "",phone: "" });
+            },
+            (error) => {
+              console.error("Mesaj gönderme hatası:", error);
+              alert("Mesaj gönderilirken bir hata oluştu.");
+            }
+          );
+      };
+
+    
     return(
         <div className="w-full h-[90lvh] p-4 pt-[10lvh] justify-center flex">
             <div className="w-full h-full flex md:flex-row flex-col md:w-2/3">
+
+            <form onSubmit={handleSubmit}>
                 <div className="w-full h-full flex flex-col border border-[#123466] rounded-tl-2xl md:rounded-bl-2xl md:rounded-tr-none rounded-tr-2xl px-6 py-8">
                     <p className="text-3xl text-[#123466] font-bold mb-4">İletişim Formu</p>
  
@@ -15,21 +78,38 @@ export default function Form () {
                                 type="text" 
                                 id="name" 
                                 name="name" 
+                                value={formData.name}
+                                onChange={handleChange}
                                 placeholder="Adınızı girin" 
                                 className="w-full p-3 mt-2 border border-gray-300 rounded-md md:text-lg text-sm focus:outline-none focus:ring-2 ring-[#123466]-blue-500"
                             />
                         </div>
     
                         <div className="mb-4">
-                            <label htmlFor="email" className="md:text-lg text-sm text-[#123466]">E-posta Adresiniz</label>
+                            <label htmlFor="phone" className="md:text-lg text-sm text-[#123466]">Telefon Numarası</label>
+                            <input 
+                                type="phone" 
+                                id="phone"  
+                                name="phone" 
+                                value={formData.phone}
+                                onChange={handleChange}
+                                placeholder="E-Telefon numarası girin" 
+                                className="w-full p-3 mt-2 border border-gray-300 rounded-md md:text-lg text-sm focus:outline-none focus:ring-2 ring-[#123466]-blue-500"
+                            />
+                        </div>
+                    </div>
+
+                    <div className="mb-4">
+                    <label htmlFor="email" className="md:text-lg text-sm text-[#123466]">E-posta Adresiniz</label>
                             <input 
                                 type="email" 
                                 id="email" 
                                 name="email" 
+                                value={formData.email}
+                                onChange={handleChange}
                                 placeholder="E-posta adresinizi girin" 
                                 className="w-full p-3 mt-2 border border-gray-300 rounded-md md:text-lg text-sm focus:outline-none focus:ring-2 ring-[#123466]-blue-500"
                             />
-                        </div>
                     </div>
                     
  
@@ -39,6 +119,8 @@ export default function Form () {
                             type="text" 
                             id="subject" 
                             name="subject" 
+                            value={formData.subject}
+                            onChange={handleChange}
                             placeholder="Konu girin" 
                             className="w-full p-3 mt-2 border border-gray-300 rounded-md md:text-lg text-sm focus:outline-none focus:ring-2 focus:ring-[#123466]-500"
                         />
@@ -49,17 +131,20 @@ export default function Form () {
                         <textarea 
                             id="message" 
                             name="message" 
+                            value={formData.message}
+                            onChange={handleChange}
                             placeholder="Mesajınızı yazın"  
                             className="w-full p-3 mt-2 border border-gray-300 rounded-md md:text-lg text-sm focus:outline-none focus:ring-2 focus:ring-[#123466] md:min-h-36 min-h-24"
                         ></textarea>
                     </div> 
                     <button 
-                        type="submit" 
+                        type="submit"  
                         className="md:w-min bg-[#123466] text-white md:px-14 md:py-4 py-3 text-xl rounded-xl hover:bg-blue-600 transition duration-300"
                     >
                         Gönder
                     </button>
                 </div>
+            </form>
 
                 <div className="md:w-[35vw] w-full md:h-full flex flex-col border border-[#123466] bg-[#123466] md:rounded-tr-2xl md:rounded-bl-none rounded-bl-2xl rounded-br-2xl justify-around py-2">
                     <div className="text-white flex flex-col items-center px-10 text-center md:pt-8 pb-4 md:gap-4 gap-2">
