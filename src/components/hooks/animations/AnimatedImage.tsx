48fbda02048f2269d8036dd571436de98280a986
Mahmut Tunahan Aktaş <mahmuttunahan0029@gmail.com>
2025-05-12 12:12:56 +0300

    last

--- src/components/hooks/animations/AnimatedImage.tsx
+++ src/components/hooks/animations/AnimatedImage.tsx
@@ -9,9 +9,10 @@ const AnimatedImage = () => {
   return (
     <motion.div
       ref={ref}
-      initial={{ opacity: 0, rotate: -10, scale: 0.8 }}
-      animate={{ opacity: 1, rotate: 0, scale: 1 }}
-      
+      initial= {{ opacity: 0, y: 100, scale:0.95 }}
+      animate=  {{ opacity:1, y: 0, scale: 1 }}
+      transition= {{ duration:0.8, ease:'easeOut' }}
+
 
     >
       <Image
